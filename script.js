0bbfc2c46a22d9829a5c5c98b7516173682d0a6d
Jung Nam <j.nam0808@gmail.com>
2021-03-20 23:23:24 -0400

    uploading initial stuff

---
+++ script.js
@@ -0,0 +1,20 @@
+var startButton = document.getElementById("start-button")
+
+
+// console.log(startButton);
+
+startButton.addEventListener("click", function(){
+    var startPage = document.getElementById("start-page")
+    startPage.style.display = "none";
+    var firstQuestion = document.getElementById("first-question")
+    firstQuestion.style.display = "block";
+    var lists = document.createElement("ul");
+    // console.log(lists)
+    var questions = document.createElement("li");
+    questions.textContent = "choice 1 ";
+    lists.append(questions);
+    firstQuestion.append(questions);
+    i = 0, i < dfjakljdsa.length
+}
+) 
+
